6aa6e9c46687866e577458613ff020fe747dceb2
Avi Ben David <avi@brandale.co.il>
2025-05-29 19:51:38 +0300

    Program fully working, now initiating bugfixes and comments

--- Exercise_3/Focus-Mode.c
+++ Exercise_3/Focus-Mode.c
@@ -12,21 +12,23 @@
 
 // Handling the first signal
 void handleSig1() {
+    printf("- Email notification is waiting.\n");
     printf("[Outcome:] The TA announced: Everyone get 100 on the exercise!\n");
 }
 
 // Handling the second signal
 void handleSig2() {
+    printf("- You have a reminder to pick up your delivery.\n");
     printf("[Outcome:] You picked it up just in time.\n");
 }
 
 // Handling the third signal
 void handleSig3() {
+    printf(" - The doorbell is ringing.\n");
     printf("[Outcome:] Food delivery is here.\n");
 }
 
 void inputResponse(char *c) {
-    printf("%c", *c);
     if (*c == '1') {
         raise(SIG1);
     }else if (*c == '2') {
@@ -62,23 +64,59 @@ void blockSigs(sigset_t *set) {
     sigaddset(set, SIG1);
     sigaddset(set, SIG2);
     sigaddset(set, SIG3);
-    return;
+    sigprocmask(SIG_SETMASK, set, NULL);
+}
+
+// Setting up signal handlers
+void sigSetup(sigset_t *set) {
+    struct sigaction usr_action;
+    usr_action.sa_handler = handleSig1;
+    usr_action.sa_mask = *set;
+    usr_action.sa_flags = 0;
+    sigaction(SIG1, &usr_action, NULL);
+
+    usr_action.sa_handler = handleSig2;
+    sigaction(SIG2, &usr_action, NULL);
+
+    usr_action.sa_handler = handleSig3;
+    sigaction(SIG3, &usr_action, NULL);
+}
+
+void handleSignals() {
+    sigset_t s;
+    sigpending(&s);
+
+    if (sigismember(&s, SIG1)) {
+        sigwaitinfo(&s, NULL);
+        handleSig1();
+    }
+    if (sigismember(&s, SIG2)) {
+        sigwaitinfo(&s, NULL);
+        handleSig2();
+    }
+    if (sigismember(&s, SIG3)) {
+        sigwaitinfo(&s, NULL);
+        handleSig3();
+    }
 }
 
 // Main Focus function
 void runFocusMode(int numOfRounds, int duration) {
+    // Setting up the blocking sets and the sigactions
     sigset_t set;
-    // Setting up the signal handlers
-    signal(SIG1, handleSig1);
-    signal(SIG2, handleSig2);
-    signal(SIG3, handleSig3);
+    sigSetup(&set);
     blockSigs(&set);
+    sigprocmask(SIG_BLOCK, &set, NULL);
     // main focus mode loop
     for (int i = 0; i < numOfRounds; ++i) {
         // Blocking the three defined signals and setting the mask on
-        sigprocmask(SIG_SETMASK, &set, NULL);
         focusModeRound(duration);
-        // Unblocking the three defined signals
-        sigprocmask(SIG_UNBLOCK, &set, NULL);
+        printf("──────────────────────────────────────────────\n"
+               "        Checking pending distractions...\n"
+               "──────────────────────────────────────────────\n");
+        handleSignals();
+        printf("──────────────────────────────────────────────\n"
+               "             Back to Focus Mode.\n"
+               "══════════════════════════════════════════════\n");
     }
 }
